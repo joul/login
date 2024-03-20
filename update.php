b0dea25c68c36648570d6c249c0a43edf41cd5d2
Joul <joul84@gmail.com>
2024-03-20 18:27:35 +0100

    update and delete statments

---
+++ update.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Change Accounts</title>
+</head>
+
+<body>
+    <h3>Change Accounts</h3>
+
+    <form action="includes/userupdate.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="Email">
+        <button>Update</button>
+    </form>
+
+    <h3>Delete Accounts</h3>
+
+    <form action="includes/userdelete.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Delete</button>
+    </form>
+</body>
+
+</html>
