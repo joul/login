f417ede055639301cd5b15f84bc7fbc7e1b787d6
Joul <joul84@gmail.com>
2024-03-20 17:47:36 +0100

    css add

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,13 @@
 <?php
+
+$dsn = "mysql:host=localhost;dbname=dani";
+$dbusername = "root";
+$dbpassword = "root";
+
+//try catch block
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+Instructions for creating database
+
 <!-- creating new table: -->
 
 CREATE TABLE comments(
